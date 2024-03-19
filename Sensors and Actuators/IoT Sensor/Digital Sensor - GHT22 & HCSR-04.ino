763741f80e5f8f22b71772c87e5c6153bab4f26c
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-03-19 19:51:30 +0700

    Update Digital Sensor - GHT22 & HCSR-04.ino

--- Sensors and Actuators/IoT Sensor/Digital Sensor - GHT22 & HCSR-04.ino
+++ Sensors and Actuators/IoT Sensor/Digital Sensor - GHT22 & HCSR-04.ino
@@ -1,8 +1,8 @@
-#include "DHTesp.h"
+#include "DHTesp.h"       //library untuk sensor dht
 
-#define DHT_PIN 15
-#define TRIG_PIN 13
-#define ECHO_PIN 14
+#define DHT_PIN 15        //pin data yang digunakan DHT
+#define TRIG_PIN 13       //pin data yang digunakan Ultrasonic
+#define ECHO_PIN 14       //pin data yang digunakan Ultrasonic
 
 DHTesp dhtSensor;
 
@@ -10,14 +10,14 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
-  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  TempAndHumidity data = dhtSensor.getTempAndHumidity();
-  Serial.println("Temp:" + String(data.temperature, 2)+ "C");
-  Serial.println("Humidity:" + String(data.humidity,1)+ "%");
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();          //meminta data bacaan sensor temperatur dan humidity
+  Serial.println("Temp:" + String(data.temperature, 2)+ "C");     //menampilkan data hasil bacaan sensor temperatur ke serial monitor
+  Serial.println("Humidity:" + String(data.humidity,1)+ "%");     //menampilkan data hasil bacaan sensor humididty ke serial monitor
   Serial.println("---");
   delay(1000); // this speeds up the simulation
 }
