5415c4e72aae9bf6a4e7609eddbdb5647daabecc
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-04-12 20:15:56 +0700

    Update UART Master.ino

--- Microcontroller (ESP32)/Digital Serial Communication in Microcontroller/UART Communication/UART Master.ino
+++ Microcontroller (ESP32)/Digital Serial Communication in Microcontroller/UART Communication/UART Master.ino
@@ -1,1 +1,15 @@
 #include <HardwareSerial.h>
+HardwareSerial SerialPort(2); 
+
+void setup()
+{
+  Serial{prt.begin(15200, SERIAL_8N1, 16, 17);
+}
+
+void loop()
+{
+  SerialPosrt.print(1);
+  delay(5000);
+  SerialPort.print(0);
+  delay(5000);
+}
